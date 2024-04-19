1f3485142087ed0ee22cf3519f86018d0c7059c2
AValenzue <zuellagraphy@gmail.com>
2024-04-19 13:00:15 +0200

    implement obstacles

--- js/main.js
+++ js/main.js
@@ -1,49 +1,95 @@
 console.log("hello world")
 
 class Player {
-    constructor(){
+    constructor() {
         this.positionX = 50;
         this.positionY = 0;
         this.width = 10;
         this.height = 10;
 
-        this.playerElm =  document.getElementById("player");
+        this.playerElm = document.getElementById("player");
         this.playerElm.style.left = this.positionX + "vw";
         this.playerElm.style.bottom = this.positionY + "vw";
 
-       this.playerElm.style.width = this.width + "vw";
+        this.playerElm.style.width = this.width + "vw";
         this.playerElm.style.height = this.height + "vh";
 
     }
 
-    doSomething(){
+    doSomething() {
         console.log("doing something..");
     }
-    moveLeft(){
+    moveLeft() {
         this.positionX--;
         this.playerElm.style.left = this.positionX + "vw";
         console.log("New Position: " + this.positionX);
 
     }
-    moveRight(){
+    moveRight() {
         this.positionX++;
         this.playerElm.style.left = this.positionX + "vw";
         console.log("New Position: " + this.positionX)
     }
+};
+
+class Obstacle {
+    constructor(){
+    this.positionX = 50;
+    this.positionY = 85;
+    this.width = 10;
+    this.height = 10;
+    this.obstacleElm = null;
+
+    this.createDomElement();
+};
+createDomElement(){
+    this.obstacleElm = document.createElement("div");
+
+    this.obstacleElm.className = "obstacle";
+    this.obstacleElm.style.left = this.positionX + "vw";
+    this.obstacleElm.style.bottom = this.positionY + "vw";
+    this.obstacleElm.style.width = this.width + "vw";
+    this.obstacleElm.style.height = this.height + "vh";
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.obstacleElm);
+}
+moveDown(){
+    this.positionY--;
+    this.obstacleElm.style.bottom  = this.positionY + "vh";
 }
 
+}
+
+
 const player = new Player();
-document.addEventListener("keydown",(e) =>{
-    if(e.code === "ArrowLeft"){
+const obstaclesArr = [];
+//const o1 = new Obstacle();
+
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+},3000)
+
+
+//move all obstacles
+setInterval(() =>  {
+    obstaclesArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+    })
+
+}, 100)
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "ArrowLeft") {
         player.moveLeft();
-    } else if (e.code === "ArrowRight"){
+    } else if (e.code === "ArrowRight") {
         player.moveRight();
 
     }
 })
 
-player.doSomething();
-player.moveLeft();
+
 
 
 
